af05cbfd0ee6482435fe57ff78c9a5717824c912
asif987patel <asif987patel@github.com>
2023-06-18 00:47:16 +0530

    refactor(CHORES): :pushpin: Update the folder name
    updated the model folder namre to models

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { StandardDeck } from './data/StandardDeck'
 import { type StandardCard } from './interfaces/StandardCard'
 import { type Hand } from './interfaces/Hand'
-import { CardDeck } from './model/CardDeck'
-import { TeenPatti } from './model/TeenPatti'
+import { CardDeck } from './models/CardDeck'
+import { TeenPatti } from './models/TeenPatti'
 
 export { TeenPatti, CardDeck, StandardDeck, type StandardCard, type Hand }
